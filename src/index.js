eae8c515c006ef01efc5cf4ee88d2c63ee06aab2
shaik akhani basha <73960716+akhaniShaik@users.noreply.github.com>
2020-12-08 01:08:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "index.css";
+import App from "App";
+import reportWebVitals from "reportWebVitals";
 //
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./services/Reducers/index";
+import rootReducer from "services/Reducers/index";
 const store = createStore(rootReducer);
 
 //
